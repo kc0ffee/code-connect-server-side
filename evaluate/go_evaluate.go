7330a3ac20829cf807dc253d4bbd487400aa476a
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2023-08-13 21:55:07 +0900

    feat: Goのコード評価の実装

--- evaluate/go_evaluate.go
+++ evaluate/go_evaluate.go
@@ -1,18 +1,43 @@
 package evaluate
 
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strings"
+)
+
 type GoEvaluator struct{}
 
 func (e *GoEvaluator) CountLines(code string) int {
-	// count lines
-	return 0
+	return strings.Count(code, "\n") + 1
 }
 
-func ParseToAST(code string) (interface{}, error) {
-	// use go/ast
-	return nil, nil
+func (e *GoEvaluator) ParseToAST(code string) (interface{}, error) {
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, "", code, parser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+	return node, nil
 }
 
-func (e *GoEvaluator) EvaluateAST(ast interface{}) *EvaluationResult {
-	//evaluate
-	return nil
+func (e *GoEvaluator) EvaluateAST(targetAst interface{}) *EvaluationResult {
+	result := &EvaluationResult{}
+	node, ok := targetAst.(*ast.File)
+	if !ok {
+		return result
+	}
+
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch n.(type) {
+		case *ast.FuncDecl:
+			result.FunctionCount++
+		case *ast.Ident:
+			result.TokenCount++
+		}
+		return true
+	})
+
+	return result
 }
