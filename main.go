0c6ca66137e04c6d811d7c7902183db5c1d96887
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-15 13:37:18 +0900

    chore : Echoの初期化などを別パッケージに分離

--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/kc0ffee/server/server"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", hello)
-
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	e := server.NewAPIServer()
+	server.StartServer(e, 10000)
 }
