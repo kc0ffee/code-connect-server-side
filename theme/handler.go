10980bc2da4dc7d3b611edb1a24553cb88fdce47
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-15 19:02:10 +0900

    feat : APIのレスポンス生成に使用する型を追加

--- theme/handler.go
+++ theme/handler.go
@@ -2,6 +2,17 @@ package handler
 
 import "github.com/labstack/echo/v4"
 
+type (
+	Theme struct {
+		ID    int    `json:"id"`    // ID はテーマのIDが格納される
+		Theme string `json:"theme"` // Theme はテーマ本文が格納される
+	}
+
+	GetThemeAPIResponse struct {
+		Themes []Theme `json:"themes"`
+	}
+)
+
 func GetThemeHandler(ctx echo.Context) error {
 	return nil
 }
