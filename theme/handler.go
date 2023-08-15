e3c7e620dcd52c89d60352c2c51383aab040b9bd
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-15 21:55:59 +0900

    feat : テーマの読み込み機能を追加

--- theme/handler.go
+++ theme/handler.go
@@ -1,6 +1,13 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
 
 type (
 	Theme struct {
@@ -12,6 +19,37 @@ type (
 		Themes []Theme `json:"themes"`
 	}
 )
+
+var (
+	Response *GetThemeAPIResponse
+)
+
+func loadTheme() (*GetThemeAPIResponse, error) {
+	f, err := os.Open("themes.json")
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		return nil, err
+	}
+	defer f.Close()
+
+	bytes, err := io.ReadAll(f)
+	var theme GetThemeAPIResponse
+	if err = json.Unmarshal(bytes, &theme); err != nil {
+		fmt.Printf("%+v\n", err)
+		return nil, err
+	}
+
+	return &theme, nil
+}
+
+func init() {
+	data, err := loadTheme()
+	if err != nil {
+		return
+	}
+	Response = data
+	return
+}
 
 func GetThemeHandler(ctx echo.Context) error {
 	return nil
