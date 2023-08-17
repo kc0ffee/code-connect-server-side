802fb7d203362b581e2e4df627247a7630d0828d
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-17 09:59:41 +0900

    refactor : databaseパッケージからAPIに関係するロジックを分離

--- code/handler.go
+++ code/handler.go
@@ -1,7 +1,44 @@
-package handler
+package code
 
-import "github.com/labstack/echo/v4"
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/kc0ffee/server/database"
+	"github.com/kc0ffee/server/models"
+	"github.com/labstack/echo/v4"
+)
 
 func PostCodeHandler(ctx echo.Context) error {
-	return nil
+	body := models.CodeData{}
+	if err := ctx.Bind(&body); err != nil {
+		return ctx.String(http.StatusBadRequest, "Invalid request")
+	}
+	timestamp, err := database.AddCodeData(&body)
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, "Failure to update database")
+	}
+
+	res, err := database.FetchCodeDataByTimestamp(timestamp)
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, "Failure to update database")
+	}
+	return ctx.JSON(http.StatusOK, res)
+}
+
+func ResultHandler(ctx echo.Context) error {
+	id, err := strconv.Atoi(ctx.QueryParam("id"))
+	if err != nil {
+		return ctx.String(http.StatusBadRequest, "Invalid id")
+	}
+	res, err := database.FetchCodeDataByID(id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return ctx.String(http.StatusBadRequest, "Not found Id")
+		}
+		return ctx.String(http.StatusInternalServerError, "Error")
+	}
+	return ctx.JSON(http.StatusOK, res)
 }
