778279c987863acbcffcaca3ebe5f7ee3da819b8
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-17 10:00:13 +0900

    refactor : APIに関するロジックを別ファイルに分離

--- database/database.go
+++ database/database.go
@@ -2,16 +2,18 @@ package database
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/labstack/echo/v4"
+	"github.com/kc0ffee/server/models"
 )
 
+var (
+	connection *sql.DB
+)
+
+// NewDBConnection create new connection to database
 func NewDBConnection(ADDRESS string, USER string, DB_NAME string) *sql.DB {
 	str := fmt.Sprintf("%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", USER, ADDRESS, DB_NAME)
 	db, err := sql.Open("mysql", str)
@@ -24,47 +26,42 @@ func NewDBConnection(ADDRESS string, USER string, DB_NAME string) *sql.DB {
 	return db
 }
 
-type ReturnData struct {
-	ID        int    `json:"id"`
-	Theme     int    `json:"themeId"`
-	Lang      string `json:"language"`
-	Code      string `json:"code"`
-	Timestamp string `json:"timeStamp"`
+// ConnectionInitialize はデータベースとの接続を初期化します。
+func ConnectionInitialize(db *sql.DB) {
+	connection = db
 }
 
-type PostData struct {
-	Theme int    `json:"themeId"`
-	Lang  string `json:"language"`
-	Code  string `json:"code"`
-}
-
-func GetResultById(c echo.Context, db *sql.DB) error {
-	id, err := strconv.Atoi(c.QueryParam("id"))
+// FetchCodeDataByID はデータベースに登録されているコードをIDを元に取得します。
+func FetchCodeDataByID(id int) (*models.CodeDataResponse, error) {
+	var result = models.CodeDataResponse{}
+	row := connection.QueryRow("SELECT * FROM results WHERE id = ?", id)
+	err := row.Scan(&result.ID, &result.Theme, &result.Lang, &result.Code, &result.Timestamp)
 	if err != nil {
-		return c.String(http.StatusBadRequest, "Invalid id")
+		return nil, err
 	}
-	var result = ReturnData{}
-	row := db.QueryRow("SELECT * FROM results WHERE id = ?", id)
-	err = row.Scan(&result.ID, &result.Theme, &result.Lang, &result.Code, &result.Timestamp)
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "Error getting result")
-	}
-	json, err := json.Marshal(result)
-	return c.String(http.StatusOK, string(json))
+	return &result, nil
 }
 
-func CreateResult(c echo.Context, db *sql.DB) error {
-	body := PostData{}
-	if err := c.Bind(&body); err != nil {
-		return c.String(http.StatusBadRequest, "Invalid request")
+// FetchCodeDataByTimestamp はtimestampを元にデータをデータベースから検索し返します
+func FetchCodeDataByTimestamp(timestamp string) (*models.CodeDataResponse, error) {
+	var result = models.CodeDataResponse{}
+	row := connection.QueryRow("SELECT * FROM results WHERE timestamp = ?", timestamp)
+	err := row.Scan(&result.ID, &result.Theme, &result.Lang, &result.Code, &result.Timestamp)
+	if err != nil {
+		return nil, err
 	}
+	return &result, nil
+}
+
+// AddCodeData はサーバーにPOSTされたデータをデータベースに保存し、保存時のtimestampを返します。
+func AddCodeData(body *models.CodeData) (string, error) {
 	now := time.Now()
 	Timestamp := now.Format("2006-01-02 15:04:05")
-	_, err := db.Exec("INSERT INTO results (theme, lang, code, timestamp) VALUES (?, ?, ?, ?)", body.Theme, body.Lang, body.Code, Timestamp)
+	_, err := connection.Exec("INSERT INTO results (theme, lang, code, timestamp) VALUES (?, ?, ?, ?)", body.Theme, body.Lang, body.Code, Timestamp)
 	if err != nil {
 		fmt.Println(err)
-		return c.String(http.StatusInternalServerError, "Error inserting into database")
+		return "", err
 	}
 	fmt.Println("Inserted into database")
-	return c.String(http.StatusOK, "Success")
+	return Timestamp, nil
 }
