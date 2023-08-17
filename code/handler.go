7d77007f5e5f26fd3cf48f1ac618259ea7dcfc03
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2023-08-18 08:43:33 +0900

    refactor: 呼び出すハンドラの修正･削除

--- code/handler.go
+++ code/handler.go
@@ -1,11 +1,8 @@
 package code
 
 import (
-	"database/sql"
 	"encoding/base64"
-	"errors"
 	"net/http"
-	"strconv"
 
 	"github.com/kc0ffee/server/database"
 	"github.com/kc0ffee/server/models"
@@ -37,29 +34,5 @@ func PostCodeHandler(ctx echo.Context) error {
 	if err != nil {
 		return ctx.String(http.StatusInternalServerError, "Failure to update database")
 	}
-	return ctx.JSON(http.StatusOK, res)
-}
-
-func ResultHandler(ctx echo.Context) error {
-	id, err := strconv.Atoi(ctx.QueryParam("id"))
-	if err != nil {
-		return ctx.String(http.StatusBadRequest, "Invalid id")
-	}
-	data, err := database.FetchCodeDataByID(id)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return ctx.String(http.StatusBadRequest, "Not found Id")
-		}
-		return ctx.String(http.StatusInternalServerError, "Error")
-	}
-
-	res := models.CodeDataResponse{
-		ID:        data.ID,
-		Theme:     data.Theme,
-		Lang:      data.Lang,
-		Code:      base64.StdEncoding.EncodeToString([]byte(data.Code)),
-		Timestamp: data.Timestamp,
-	}
-
 	return ctx.JSON(http.StatusOK, res)
 }
